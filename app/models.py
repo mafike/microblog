8c4216b8ceaf03a196a43bcad3b98e750001c617
mafike <bottler.octanes.0p@icloud.com>
2025-07-23 14:49:25 -0400

    translation

--- app/models.py
+++ app/models.py
@@ -1,13 +1,13 @@
 from datetime import datetime, timezone
 from hashlib import md5
+from time import time
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db, login, app
-from time import time
 import jwt
+from app import app, db, login
 
 
 followers = sa.Table(
@@ -91,7 +91,7 @@ class User(UserMixin, db.Model):
             .group_by(Post)
             .order_by(Post.timestamp.desc())
         )
-    
+
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
@@ -102,7 +102,7 @@ class User(UserMixin, db.Model):
         try:
             id = jwt.decode(token, app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
-        except:
+        except Exception:
             return
         return db.session.get(User, id)
 
